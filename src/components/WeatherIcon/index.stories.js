42ad18600686a10983d5cdefe004e773917114d3
lukePeavey <1williampeavey@gmail.com>
2017-09-30 13:21:37 -0400

    Create WeatherIcon component

---
+++ src/components/WeatherIcon/index.stories.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
+import { storiesOf } from '@storybook/react'
+import WeatherIcon from '.'
+
+const req = require.context('../../../public/weather-icons/', false, /.svg$/)
+const dayIcons = req.keys().filter(filename => !filename.includes('nt_'))
+const nightIcons = req.keys().filter(filename => filename.includes('nt'))
+
+storiesOf('Weather Icons', module).add('default', () => (
+  <div className="storyWrapper">
+    <div className={css(styles.container)}>
+      <h3>Day Weather Icons</h3>
+      {dayIcons.map(name => (
+        <WeatherIcon className={css(styles.weatherIcon)} name={name.replace('.svg', '')} />
+      ))}
+    </div>
+    <div className={css(styles.container)}>
+      <h3>Night Weather Icons</h3>
+      {nightIcons.map(name => (
+        <WeatherIcon className={css(styles.weatherIcon)} name={name.replace('.svg', '')} />
+      ))}
+    </div>
+  </div>
+))
+
+const styles = StyleSheet.create({
+  weatherIcon: {
+    width: 60,
+    margin: 15
+  },
+  container: {
+    width: '100%',
+    marginBottom: '40'
+  }
+})
