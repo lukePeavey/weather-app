f190ee3c912e30a7da7b0911b31e0f237076427a
lukePeavey <1williampeavey@gmail.com>
2017-10-18 07:46:23 -0400

    Update redux store

--- src/store/configure.js
+++ src/store/configure.js
@@ -5,12 +5,12 @@ import createHistory from 'history/createBrowserHistory'
 import rootReducer from './reducers'
 import rootSaga from './sagas'
 
-const initialState = {}
-const enhancers = []
-const sagaMiddleware = createSagaMiddleware()
-const middleware = [sagaMiddleware, routerMiddleware(history)]
-
 export default function configureStore(initialState = {}) {
+  const history = createHistory()
+  const enhancers = []
+  const sagaMiddleware = createSagaMiddleware()
+  const middleware = [sagaMiddleware, routerMiddleware(history)]
+
   if (process.env.NODE_ENV === 'development') {
     const devToolsExtension = window.devToolsExtension
     if (typeof devToolsExtension === 'function') {
@@ -18,11 +18,7 @@ export default function configureStore(initialState = {}) {
     }
   }
 
-  const history = createHistory()
-  const composedEnhancers = compose(
-    applyMiddleware(...middleware),
-    ...enhancers
-  )
+  const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers)
   const store = createStore(rootReducer, initialState, composedEnhancers)
   sagaMiddleware.run(rootSaga)
   return { store, history }
