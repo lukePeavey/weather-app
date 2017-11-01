05b5d023b0780e086fb52ec4c487d22d6913fee9
Luke Peavey <1williampeavey@gmail.com>
2017-11-01 00:13:24 -0400

    Add material-ui theme provider to app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import theme from './styles/theme'
 import configureStore from './store/configure'
-import App from './components/App'
 import registerServiceWorker from './utils/registerServiceWorker'
+import App from './components/App'
 import './styles/index.css'
 
-const { store, history } = configureStore()
+const store = configureStore()
 
 render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
