0f80f2b909c4deb1052938414429bb415a2749b3
Luke Peavey <1williampeavey@gmail.com>
2017-11-03 01:15:20 -0400

    [LoginForm] Refactor container component

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -4,18 +4,23 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 import LoginForm from '../components/LoginForm'
 import { loginRequest } from '../store/auth/actions'
+import { Link } from 'react-router-dom'
 
 /**
- * The login form container provides event event handlers to
- * the login form, which a wrapped redux-form component.
+ * Container for the the LoginForm component. Form state is managed with
+ * redux-form. The container provides event handlers for form submission
+ * and other events.
+ *
+ * @todo Add synchronous field level validation
  */
 class LoginFormContainer extends Component {
   /**
    * onSubmit handler for the login form.
-   * This dispatches the loginRequest action when the login form is submitted.
-   * The
+   * This dispatches the loginRequest action when the login form is submitted,
+   * passing it the form data and resolve/reject callbacks as the payload.
+   * The login saga will handle the actual authentication process.
    */
-  mySubmit = credentials => {
+  onSubmit = credentials => {
     return new Promise((resolve, reject) => {
       this.props.dispatch(loginRequest({ credentials, resolve, reject }))
     })
@@ -24,9 +29,12 @@ class LoginFormContainer extends Component {
   render() {
     const { from } = this.props.location.state || { from: { pathname: '/' } }
     if (this.props.submitSucceeded) {
+      // When form submission completes successfully, redirect back to the
+      // previous location. This will take the user back to where they were
+      // before they navigated to the login screen.
       return <Redirect to={from} />
     }
-    return <LoginForm {...this.props} mySubmit={this.mySubmit} />
+    return <LoginForm onSubmit={this.onSubmit} {...this.props} />
   }
 }
 
