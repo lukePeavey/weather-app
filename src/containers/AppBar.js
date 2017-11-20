129089411f1886b245a9776181e8bf6737b18b6b
lukePeavey <1williampeavey@gmail.com>
2017-11-20 00:00:08 -0500

    [Appbar] React router to handle weather views

--- src/containers/AppBar.js
+++ src/containers/AppBar.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import AppBar from '../components/AppBar'
-import { setWeatherView } from '../store/weather/actions'
 import * as fromState from '../store/selectors'
 
 class AppBarContainer extends Component {
   state = {
     navDrawerOpen: false,
-    placesMenuActive: false
+    placesMenuActive: false,
+    activeTab: 0
   }
 
+  tabs = [
+    { slug: 'today', label: 'Current', index: 0 },
+    { slug: 'tomorrow', label: 'Tomorrow', index: 1 },
+    { slug: 'forecast', label: '10 Days', index: 2 }
+  ]
+
   handleTabChange = (event, value) => {
-    this.props.dispatch.setWeatherView(value)
+    const { history } = this.props
+    const tab = this.tabs.find(tab => tab.index === value)
+    if (tab) {
+      this.setState({ activeTab: value })
+      history.push(`/${tab.slug}`)
+    }
   }
 
   toggleNavDrawer = open => () => {
@@ -28,6 +39,7 @@ class AppBarContainer extends Component {
       <AppBar
         {...this.props}
         {...this.state}
+        tabs={this.tabs}
         toggleNavDrawer={this.toggleNavDrawer}
         togglePlacesMenu={this.togglePlacesMenu}
         handleTabChange={this.handleTabChange}
@@ -40,13 +52,9 @@ class AppBarContainer extends Component {
 // prettier-ignore
 const mapDispatchToProps = dispatch => ({
   dispatch: bindActionCreators({
-    setWeatherView,
   }, dispatch)
 })
 
-const mapStateToProps = (state, ownProps) => ({
-  tabs: fromState.getWeatherViews(state),
-  activeTab: fromState.getActiveView(state)
-})
+const mapStateToProps = (state, ownProps) => ({})
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppBarContainer)
