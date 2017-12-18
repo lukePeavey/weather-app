393dab84641b12f6334909c5049d9ecd8ae56a42
lukePeavey <1williampeavey@gmail.com>
2017-12-18 03:15:27 -0500

    [Auth] Refactor sagas

--- src/store/auth/sagas.js
+++ src/store/auth/sagas.js
@@ -12,7 +12,6 @@ import {
 import { SubmissionError } from 'redux-form'
 import api from '../../utils/api'
 import * as fromState from '../selectors'
-import * as notificationActions from '../notifications/actions'
 import * as actions from './actions'
 import * as types from './constants'
 
@@ -26,10 +25,7 @@ function* authenticate({ credentials, resolve, reject }) {
   try {
     const { token, user } = yield call(api.post, '/auth/login', credentials)
     yield put(actions.loginSuccess({ token, user }))
-    resolve()
-    // dispatch a UI notification to let the user know they logged in successfully
-    yield put(notificationActions.addNotification({ message: 'You are logged in.' }))
-    return token
+    return resolve('Success')
   } catch (error) {
     yield put(actions.loginFail())
     reject(
@@ -45,52 +41,38 @@ function* authenticate({ credentials, resolve, reject }) {
 }
 
 /**
- * This is watcher saga that uses lower level methods like take and fork to
- * implement the login flow. It handles the entire cycle of login -> logout
- * in a single function. Its loosely based on an example from saga's official
- * documentation.
+ * This is a watcher saga that handles the login flow. It uses saga's lower-
+ * level methods `take` and `fork`. (adaped from an example in saga's docs)
+ *
+ * NOTE: Because the app uses token based authentication, the user might already
+ * be logged in when they launch the app. So step 1 listens for two actions:
+ * LOGIN_REQUEST (dispatched when login form is submitted) or FETCH_USER_SUCCESS
+ * (dispatched when the authenticated user has been retrieved form database).
  */
 function* loginFlow() {
   while (true) {
-    // 1. Wait for the LOGIN_REQUEST or FETCH_USER_SUCCESS actions to be
-    // dispatched. If the user is already logged in when the app loads, for
-    // example after refreshing the page, the LOGIN_REQUEST action wont be
-    // dispatched in that life cycle (because user is already logged in). in
-    // that case, FETCH_USER_SUCCESS will be dispatched when the authenticated
-    // user has been fetched from server.
+    // 1. Wait for user to login
     const loginAction = yield take([types.LOGIN_REQUEST, types.FETCH_USER_SUCCESS])
-    let task = null
+    let authenticationTask = null
 
+    // If LOGIN_REQUEST was dispatched, run the authenticate task in a separate thread.
     if (loginAction.type === types.LOGIN_REQUEST) {
-      // 2. When LOGIN_REQUEST is dispatched, run the authenticate task to handle
-      // the login process. The `fork` method is non-blocking, so the loginFlow
-      // saga will continue to execute while the authenticate task is running.
-      task = yield fork(authenticate, loginAction.payload)
+      authenticationTask = yield fork(authenticate, loginAction.payload)
     }
 
-    // 3. Wait for LOGIN_FAIL or LOGOUT actions to be dispatched.
-    const logoutAction = yield take([types.LOGIN_FAIL, types.LOGOUT])
+    // 2. Wait for user to logout, or the login attempt to fail
+    yield take([types.LOGIN_FAIL, types.LOGOUT])
 
-    if (task && logoutAction.type === types.LOGOUT) {
-      // If the user attempted to log out while the authenticate task was still running,
-      // cancel it. (ie if user logged in, then tried to logout before the login process
-      // was complete. this is unlikely, but possible)
-      yield cancel(task)
-    }
+    // If the authentication task in still in progress cancel it
+    if (authenticationTask) yield cancel(authenticationTask)
 
-    // 4. Make an api request to log the user out. (this has no effect if the user is not
-    // currently logged in)
+    // Finally, make API call to log the user out on the back end
     yield call(api.post, '/auth/logout')
-
-    if (logoutAction.type === types.LOGOUT) {
-      // When the user logs out, dispatch a UI notification to indicate successful logout
-      yield put(notificationActions.addNotification({ message: 'You are logged out.' }))
-    }
   }
 }
 
 /**
- * Worker saga for the signup process. Works the same way as the login saga,
+ * Worker saga to handle the signup process.
  * its triggered from the signup form and receives resolve/reject callbacks
  * from the form's onSubmit method.
  * @param {Object} payload
