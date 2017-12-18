d4370827672fec09dda7b66ec5ce653caf798f7e
lukePeavey <1williampeavey@gmail.com>
2017-12-18 02:30:28 -0500

    [Places] Refactor places sagas

--- src/store/places/sagas.js
+++ src/store/places/sagas.js
@@ -1,37 +1,44 @@
 import { takeLatest, put, call, all, select } from 'redux-saga/effects'
 import api from '../../utils/api'
 import * as actions from './actions'
+import * as weatherAction from '../weather/actions'
 import * as types from './constants'
 import * as fromState from '../selectors'
 
+/**
+ * Fetches details for a location from the API and stores them in the state
+ */
 export function* fetchPlaceDetails(placeid) {
   try {
+    // Check to see if the location is already stored in state
     let place = yield select(fromState.getPlace, placeid)
     if (!place) {
       yield put(actions.fetchPlaceDetailsRequest(placeid))
       place = yield call(api.get, '/places/details', { placeid })
       yield put(actions.fetchPlaceDetailsSuccess(place))
     }
+    return place
   } catch (error) {}
 }
 /**
- * Handles the process of selecting the location for which to show weather.
+ * Sets the active location for weather forecast
  *
- * This process can be initiated by selecting a location via the searchbar
- * or saved locations dropdown. There are three steps to this process:
- * 1. set activePlaceId to the selected location
- * 2. Fetch the place details (from Google Places API) including coordinates
- * which are required to get weather data. Depending on how the location
- * was selected (ie searchbar vs saved locations) the place details might
- * already be stored in state, so the saga will check there first.
- * 3. Once step 2 is complete, dispatch an action to fetch weather data for
- * the selected location, passing the the place details as payload. Fetching
- * the weather data is handled in a separate saga in the weather module.
+ * This action can be triggered by selecting a location from the searchbar
+ * or the saved locations dropdown. It involves several asynchronous actions
+ * that must be performed sequentially. There are three steps:
+ *
+ * 1. Set `activePlaceId` to the selected location id.
+ * 2. Fetch the place details from the API. This provides the geographic
+ *    coordinates that will be used to get weather data for the selected place.
+ * 3. Fetch weather data for the location using the coordinates from step 2.
  */
 function* selectLocation({ payload: { placeid } }) {
   yield put(actions.setActivePlaceId(placeid))
   try {
-    yield fetchPlaceDetails(placeid)
+    const place = yield fetchPlaceDetails(placeid)
+    if (place) {
+      yield put(weatherAction.fetchWeatherRequest(place, ['current', 'days']))
+    }
   } catch (error) {
     yield put({ type: 'SELECT_LOCATION_FAILED' })
   }
