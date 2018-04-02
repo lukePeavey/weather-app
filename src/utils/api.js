1d2384ef50dfd2bc1c15bac2a8394c7d4d3c1739
Luke Peavey <1williampeavey@gmail.com>
2018-04-01 23:56:45 -0400

    Minor cleanup

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,21 +2,34 @@ import { isObject } from 'lodash'
 import { stringify } from 'query-string'
 import config from '../config'
 
-const request = function({ endpoint, method, data, params }) {
+/**
+ * Makes api requests using native fetch.
+ * @param {object} options
+ * @param options.endpoint {string} required api endpoint ie '/weather/'
+ * @param options.params {object} query parameters
+ * @param options.data {object} data to send with the request
+ */
+const request = function({ endpoint, params, method, data }) {
   const query = params ? `?${stringify(params)}` : ''
   const url = `${config.API_URL}${endpoint}${query}`
   let settings = {
     method,
-    credentials: 'include',
+    credentials: 'include', // this is required so fetch will include cookies in request
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
     }
   }
-  if (isObject(data)) settings.body = JSON.stringify(data)
+  if (isObject(data)) {
+    // Convert form data to json and include it in the body of the request
+    settings.body = JSON.stringify(data)
+  }
   return fetch(url, settings)
 }
 
+/**
+ * Simple API  client
+ */
 export default {
   get: async (endpoint, params) => {
     let response = await request({ endpoint, params, method: 'get' })
